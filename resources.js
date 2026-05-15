ef3e16fb8a71323cf687c2c5138ce429ebb659d9
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2026-05-14 23:23:47 -0700

    data: finished up to CSP (wip)

--- resources.js
+++ resources.js
@@ -122,5 +122,158 @@ const RESOURCES = {
         }
       ]
     }
+  },
+  "cs": {
+    label: "CS & CSP",
+    description: "solve your cube into a cubeshape, and avoid parity",
+    "cs": {
+      label: "CS",
+      bait: "solve cubeshape",
+      description: "Be more efficient at cubeshape.</br>NOTE: once you get used to how to " +
+        "think for CS, going to watch any of the CSP tutorials and picking the better " +
+        "alg would be much more efficient.",
+      resources: [
+        {
+          title: "Scallop/Kite Tutorial",
+          url: "https://youtu.be/-fYJTvNSCdo",
+          type: "video",
+          credit: "- Cube Master",
+          path: "sk",
+          description: "Explains how to the thinking behind CS. Make sure to experiment " +
+            "with the other way to solve scallops and scallop/kite, so you don't get " +
+            "stuck on algs you don't like.",
+          featured: true
+        },
+        {
+          title: "Full CS Tutorial",
+          url: "https://youtu.be/GwlHUS7e-go",
+          type: "video",
+          credit: "- Cube Master",
+          path: "full",
+          description: "A little repetitive, but again check for the other way to solve " +
+            "all the 3 slicers, so you don't get stuck on algs you don't like.",
+          featured: true
+        },
+        {
+          title: "Full CS Flowchart",
+          url: "https://imgur.com/a/full-cubeshape-chart-square-1-toXxlEv",
+          type: "image",
+          credit: "- u/olimo",
+          path: "chart",
+          description: "A neat flowchart that has questionable algs and is impossible to " +
+            "read. But it does teach you how to think about CS, and where the names came from."
+        },
+        {
+          title: "CS Trainer",
+          url: "https://cstimer.net",
+          type: "trainer",
+          credit: "- Chen Shuang",
+          path: "cstimer",
+          description: "Just the CSTimer you know and love. Switch the scramble type to " +
+            "\"Square-1\" for the left dropdown and \"CSP\" for the right dropdown, and " +
+            "use the gear icon immediately to the right to choose CS cases.",
+          featured: true
+        },
+      ]
+    },
+    "csp": {
+      label: "CSP",
+      bait: "Avoid parity by solving cubeshape differently. Basis of VDB and Lin.",
+      description: "By tracing the parity of the cube during inspection, you can distinguish" +
+        "between the two parities of the cube, and do different algs for this. This " +
+        "(theoretically) ensures you NEVER get parity.",
+      resources: [
+        {
+          title: "CSP Tutorials",
+          url: "https://www.youtube.com/watch?v=GNmrLHw86ss&list=PL8Izpzk-6zizm3E5icawyJ637EJyaIwrB&index=1",
+          type: "video",
+          credit: "- Matt",
+          path: "matt",
+          description: "Shapes sorted by frequency. Includes a full tutorial and explains " +
+            "the algs.",
+          featured: true
+        },
+        {
+          title: "CSP Trainer + more",
+          url: "https://squan-go.web.app/csp/",
+          type: "trainer",
+          credit: "- Abid",
+          path: "squango",
+          description: "Does everything you can imagine but the teaching. Integrates " +
+            "seamlessly with Matt's tutorials. Provides trainer, progress tracker, " +
+            "parity tracer, and more.",
+          featured: true
+        },
+        {
+          title: "CSP Sheet",
+          url: "https://docs.google.com/spreadsheets/d/1F627bYHNPyVpD-zqvEXeq7W1_Xod27wnQ6fPMaGVXmg/edit",
+          type: "doc/sheet",
+          credit: "- BlueAcidBall",
+          path: "bab",
+          description: "Shapes sorted by frequency. Includes everything apart from tracing. " +
+            "Algorithms displayed with images."
+        },
+        {
+          title: "CSP Site",
+          url: "https://wo0fle.github.io/square-1/cs/ezcsp",
+          type: "website",
+          credit: "- Seby",
+          path: "seby",
+          description: "Shapes sorted by frequency. Includes a full tutorial. Has non-standard" +
+            "naming though."
+        },
+        {
+          title: "CSP Sheet",
+          url: "https://docs.google.com/spreadsheets/d/1eB4gjEUYEOWKnMvimfReXuzsWBPhLqbMp_tgWrFO3DA/edit",
+          type: "doc/sheet",
+          credit: "- Matt",
+          path: "matt2",
+          description: "Shapes sorted by frequency. A probably outdated version of the " +
+            "CSP tutorials. Includes everything apart from tracing."
+        },
+        {
+          title: "CSP Doc",
+          url: "https://docs.google.com/document/d/1_PWDmEZKot5MV7bJ1qWDxhTQyLBQGKqgsBeh3FgW6tU/edit",
+          type: "doc/sheet",
+          credit: "- Lorenzo",
+          path: "lorenzo",
+          description: "Shapes organized by symmetry. Includes a full tutorial."
+        },
+        {
+          title: "CSP Doc",
+          url: "https://drive.google.com/file/d/1fm7cZEYs46LzrWroh4Vd6AJFsGDJ9mnq/view",
+          type: "doc/sheet",
+          credit: "- SAC2",
+          path: "sac2",
+          description: "Shapes sorted by shape. Includes everything apart from tracing. " +
+            "Clean graphics, but includes no explanation of the algs."
+        },
+        {
+          title: "CSP Sheet",
+          url: "https://docs.google.com/spreadsheets/d/1SvFjP5UiiJYJ4Wls341NmP_PKEd2skFyMXHm6Qsk2go/edit",
+          type: "doc/sheet",
+          credit: "- Marco",
+          path: "marco",
+          description: "Shapes sorted by shape. Includes everything apart from tracing. " +
+            "Outdated and not maintained."
+        },
+        {
+          title: "CSP Tutorials",
+          url: "https://youtube.com/playlist?list=PLF0mfn_ogsH_mGKR9j4mR-USh7CnBxec-",
+          type: "video",
+          credit: "- Cube Master",
+          path: "cm",
+          description: "Shapes sorted by shape. Includes a full tutorial, but outdated."
+        },
+        {
+          title: "CSP Tutorial",
+          url: "https://youtu.be/pXXr87kwZuI",
+          type: "video",
+          credit: "- Eva",
+          path: "eva",
+          description: "Only teaches tracing. Outdated."
+        }
+      ]
+    }
   }
 };
